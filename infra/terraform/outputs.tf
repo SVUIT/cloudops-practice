fb2b82186dda3f9a5ca5b4e3061d09c83c392334
AndrewDoan01 <23520006@gm.uit.edu.vn>
2025-08-16 09:57:15 +0700

    feat(terraform): create Azure Traffic Manager

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -12,3 +12,11 @@ output "resource_group_locations" {
   description = "Locations of created resource groups"
   value       = { for k, v in module.resource_groups : k => v.location }
 }
+
+output "traffic_manager_profile_name" {
+  value = azurerm_traffic_manager_profile.tm_profile.name
+}
+
+output "traffic_manager_profile_fqdn" {
+  value = azurerm_traffic_manager_profile.tm_profile.fqdn
+}
